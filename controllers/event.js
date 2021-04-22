55bc258bb702360997ba4a0f5f6d8e1f00a7c5ee
You know me <hiyouknowme1515@gmail.com>
2021-04-22 19:58:26 +0530

    update event delete event implemented

--- controllers/event.js
+++ controllers/event.js
@@ -1,6 +1,26 @@
+const  _= require('lodash')
+
 const { Event }= require('../models/event')
 
-getEvents = async(req, res) => {
+
+const eventById = (req, res, next, id) => {
+    console.log(id)
+    Event.findById(id)
+        .populate('postedBy', '_id name')
+        .select('_id EventName EventType EventOrganiser DateOfEvent EventDescription created ')
+        .exec((err, event) => {
+            if(err || !event ){
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            req.event = event
+            console.log(req.event)
+            next()
+        })
+}
+
+const getEvents = async(req, res) => {
    const events = await Event.find()
         .populate("postedBy", "_id name email")
    .then( events => {
@@ -11,7 +31,7 @@ getEvents = async(req, res) => {
    )
 }
 
-createEvent = async(req, res) => {
+const createEvent = async(req, res) => {
     
     const event = await new Event(req.body)
 
@@ -24,7 +44,70 @@ createEvent = async(req, res) => {
     });
 };
 
+const eventsByUser = (req, res) => {
+    Event.find({ postedBy: req.profile._id })
+        .populate('postedBy', '_id name')
+        .select('_id EventName EventType EventOrganiser EventDescription DateOfEvent')
+        .sort('_created')
+        .exec((err, events) => {
+            if(err){
+                return res.status(400).json({
+                    error: err
+                })
+               
+            }
+            res.json(events)
+        })
+}
+
+const isEventOrganiser = (req, res, next ) => {
+    let sameUser = req.event && req.auth && req.event.postedBy._id == req.auth._id
+   
+    if(!sameUser){
+        return res.status(403).json({
+            error: 'User is not authorized'
+        })
+    }
+
+    next()
+}
+
+const updateEvent = (req, res, next ) => {
+    let event = req.event
+    event = _.extend(event , req.body)
+    event.updated = Date.now()
+    event.save(err => {
+        if(err){
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json(event)
+    })
+}
+
+const deleteEvent = (req, res) => {
+    let event = req.event
+    event.remove((err, event) => {
+        if(err){
+            return res.status(400).json({
+                error: err
+            })
+        }
+        res.json({
+            message: "Event deleted successfully"
+        })
+    })
+}
+
+
+
 module.exports = {
     getEvents,
-    createEvent
+    createEvent,
+    deleteEvent,
+    eventById,
+    updateEvent,
+    isEventOrganiser,
+    eventsByUser
 }
--- routes/event.js
+++ routes/event.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const {userById} = require('../controllers/user')
 
-const {getEvents, createEvent} = require('../controllers/event')
+const {
+    getEvents, 
+    createEvent,
+    isEventOrganiser,
+    updateEvent,
+    eventById,
+    deleteEvent,
+    eventsByUser
+} = require('../controllers/event')
 const { createEventValidator } = require('../validators')
 const { requireLogin } = require('../controllers/auth')
 const router = express.Router()
 
 router.get('/', getEvents)
 router.post('/event/new/:userId',requireLogin, createEventValidator, createEvent)
+router.put('/event/:eventId', requireLogin, isEventOrganiser, updateEvent)
+router.delete('/event/:eventId', requireLogin, isEventOrganiser, deleteEvent)
+router.get('/events/by/:userId', requireLogin, eventsByUser)
+
 router.param('userId', userById);
+router.param('eventId', eventById)
 
 module.exports = router
